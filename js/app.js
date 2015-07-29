08594588bd6fb6944495424c67917359d5e403a5
Ankur <aksinghmt@thoughtworks.com>
2015-07-29 19:24:33 +0530

    adding A controller as notesCtrl and making a http get request

--- js/app.js
+++ js/app.js
@@ -1,21 +1,26 @@
 angular.module('NoteWrangler', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider
-
+    .when('/', {
+      redirectTo: '/users'
+    })
     .when('/notes', {
-      templateUrl: 'templates/pages/notes/index.html'
+      templateUrl: 'templates/pages/notes/index.html',
+      controller: function($http) {
+    var controller = this;
+    $http({method: 'GET', url: '/notes'}).success(function(data) {
+      controller.notes = data;
+    });
+  },
+  controllerAs: 'notesCtrl'
     })
     .when('/users', {
       templateUrl: 'templates/pages/users/index.html'
     })
-   .when('/notes/new', {
-     templateUrl: 'templates/pages/notes/edit.html'
-   })
-  .when('/', {
-    redirectTo: '/users'
-  })
-  .otherwise({
-    redirectTo: '/notes'
-  });
- 
+    .when('/notes/new', {
+      templateUrl: 'templates/pages/notes/edit.html'
+    })
+    .otherwise({
+      redirectTo: '/notes'
+    });
 }]);
