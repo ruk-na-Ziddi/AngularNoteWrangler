89c1d95254ceb1853e767ed84fd5638e75a5c60b
Ankur <aksinghmt@thoughtworks.com>
2015-07-29 19:49:36 +0530

    adding a exetrnal controller as newNotesCtrl for '/notes/new'

--- js/app.js
+++ js/app.js
@@ -6,19 +6,21 @@ angular.module('NoteWrangler', ['ngRoute'])
     })
     .when('/notes', {
       templateUrl: 'templates/pages/notes/index.html',
-      controller: function($http) {
-    var controller = this;
-    $http({method: 'GET', url: '/notes'}).success(function(data) {
-      controller.notes = data;
-    });
-  },
-  controllerAs: 'notesCtrl'
+      controller: function($http){
+        var controller = this;
+        $http({method: 'GET', url: '/notes'}).success(function(data){
+          controller.notes = data;
+        });
+      },
+      controllerAs: 'notesCtrl'
     })
     .when('/users', {
       templateUrl: 'templates/pages/users/index.html'
     })
     .when('/notes/new', {
-      templateUrl: 'templates/pages/notes/edit.html'
+      templateUrl: 'templates/pages/notes/edit.html',
+      controller: 'NotesCreateController',
+      controllerAs: 'newNotesCtrl'
     })
     .otherwise({
       redirectTo: '/notes'
