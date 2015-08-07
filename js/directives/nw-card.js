bee028aabd043f4ca8abc0e65a9285d8a211a9f2
Ankur <aksinghmt@thoughtworks.com>
2015-08-07 19:29:53 +0530

    adding two way binding for tweeted

--- js/directives/nw-card.js
+++ js/directives/nw-card.js
@@ -4,7 +4,8 @@ angular.module('NoteWrangler').directive('nwCard', [function() {
     templateUrl: 'templates/directives/nw-card.html',
     scope: {
       header: "=",
-      description: "="
+      description: "=",
+      tweeted: '='
     },
     link: function(scope, element){
       if(scope.tweeted) {
